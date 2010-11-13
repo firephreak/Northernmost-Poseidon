ab8d87a53a6f44471f48ed452e38efe973c339d7
Dylan Canfield <firephreak@firephreak.ath.cx>
2010-11-13 13:31:55 -0700

    Changed pom.xml to get rid of twitter deps

--- src/main/java/cx/it/northernmostposeidon/songcloud/servlet/AuthServlet.java
+++ src/main/java/cx/it/northernmostposeidon/songcloud/servlet/AuthServlet.java
@@ -11,8 +11,6 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-import static cx.it.northernmostposeidon.songcloud.TwitterUtils.newTwitter;
-import static cx.it.northernmostposeidon.songcloud.TwitterUtils.setAccessToken;
 
 public class AuthServlet extends HttpServlet {
     public static final String AUTH_FORM_VIEW = "/WEB-INF/jsp/authForm.jsp";
@@ -21,30 +19,4 @@ public class AuthServlet extends HttpServlet {
     public static final String REQUEST_TOKEN_ATTRIBUTE = "requestToken";
     public static final String PIN_PARAM = "pin";
 
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        Twitter twitter = newTwitter();
-        try {
-            RequestToken requestToken = twitter.getOAuthRequestToken();
-            req.getSession().setAttribute(REQUEST_TOKEN_ATTRIBUTE, requestToken);
-            req.setAttribute(AUTH_URL_ATTRIBUTE, requestToken.getAuthorizationURL());
-            req.getRequestDispatcher(AUTH_FORM_VIEW).forward(req, resp);
-        } catch (TwitterException e) {
-            resp.sendError(e.getStatusCode(), e.getMessage());
-        }
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        Twitter twitter = newTwitter();
-        try {
-            HttpSession session = req.getSession();
-            RequestToken requestToken = (RequestToken) session.getAttribute(REQUEST_TOKEN_ATTRIBUTE);
-            session.removeAttribute(REQUEST_TOKEN_ATTRIBUTE);
-            setAccessToken(session, twitter.getOAuthAccessToken(requestToken, req.getParameter(PIN_PARAM)));
-            req.getRequestDispatcher(AUTH_RESULTS_VIEW).forward(req, resp);
-        } catch (TwitterException e) {
-            resp.sendError(e.getStatusCode(), e.getMessage());
-        }
-    }
 }
