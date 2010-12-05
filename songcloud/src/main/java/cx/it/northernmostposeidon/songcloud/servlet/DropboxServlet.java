5a52932b11b9127a97b6c3c75f47dbbcfeb1f964
Eric Horacek <horacek.eric@gmail.com>
2010-12-05 14:03:49 -0700

    added path parsing functionality to dropbox servlet

--- songcloud/src/main/java/cx/it/northernmostposeidon/songcloud/servlet/DropboxServlet.java
+++ songcloud/src/main/java/cx/it/northernmostposeidon/songcloud/servlet/DropboxServlet.java
@@ -8,7 +8,14 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Map;
+import java.util.*;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONValue;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.ContainerFactory;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 public class DropboxServlet extends HttpServlet {
 
@@ -19,13 +26,33 @@ public class DropboxServlet extends HttpServlet {
         
 		try {                     
 			DropboxClient client = getClient(req.getSession());
-			Map accountInfo = client.accountInfo(true, null);
-		    System.out.println(accountInfo);
 
-            Map res = client.metadata("dropbox", "/Public/music", 100, null, true, false, null);
-            System.out.println(res);            
-			
+            Map accountInfo = client.accountInfo(true, null);
+            String uid = (((Map)accountInfo.get("body")).get("uid")).toString();
+            System.out.println(uid);
+
+            Map metadata = client.metadata("dropbox", "/Public/music", 100, null, true, false, null);
+
+            //System.out.println(((JSONArray)(metadata.get("contents"))).length());
+
+            ArrayList<String> songs = new ArrayList<String>();
+
+            for (Object o : (JSONArray)(metadata.get("contents")))
+            {
+                String tempString = ((String)((Map)o).get("path"));
+
+                tempString = tempString.substring(7);
+
+                tempString = "https://dl.dropbox.com/u/" + uid + tempString;
+
+                songs.add(tempString);
+            }
+
+            System.out.println(songs);
+
+            req.setAttribute("songs", songs.toArray());
 			req.getRequestDispatcher(PLAYER_VIEW).forward(req, resp);
+            
         } catch (Exception e) {
 			e.printStackTrace();
         }
